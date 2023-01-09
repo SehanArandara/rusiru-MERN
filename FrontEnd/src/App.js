5a20c6c9a22c9e3abd1e5c6cc9dfaf6494fb9fc0
Sehan Arandara <99310968+SehanArandara@users.noreply.github.com>
2023-01-09 21:23:51 +0530

    Add files via upload

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import NavBar from './Components/NavBar';
+import AddStudent from './Components/AddStudent';
+import AllStudents from './Components/AllStudents';
+import EditStudent from './Components/EditStudent';
+import {BrowserRouter,Route,Routes} from "react-router-dom";     // import this file to do router
+
+/*https://stackoverflow.com/questions/71303779/react-route-is-not-working-react-router*/
+// do routing as below because this is new version
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/add" element={<AddStudent />} />
+        </Routes>
+        <Routes>
+          <Route path="/edit/:id" element={<EditStudent />} />
+        </Routes>
+        <Routes>
+          <Route path="/" element={<AllStudents />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
